51e8d3db668fdea93f65619546d5696e7db949c3
Gregoire Hibon <spermo9@gmail.com>
2011-06-04 19:09:21 +0800

    1ere version

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  # def logo
+  #   image_tag("logo.png", :alt => "Sample App", :class => "round")
+  # end
+
+  # Return a title on a per-page basis.
+  def title
+    base_title = "RoR"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Automoto::Application.routes.draw do
   
   root :to => 'pages#home'
-  get "pages/home"
+  # get "pages/home"
+  #   get "pages/contact"
+  #   get "pages/about"
+  
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
 
-  get "pages/contact"
-
-  get "pages/about"
 
   
   
