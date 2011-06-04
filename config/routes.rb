cc7bc912419bd4a4fc4ec9fa4cea94f205e5a28c
Gregoire Hibon <spermo9@gmail.com>
2011-06-04 23:06:31 +0800

    siteduzero

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Automoto::Application.routes.draw do
   
+  get "users/new"
+
   root :to => 'pages#home'
-  # get "pages/home"
-  #   get "pages/contact"
-  #   get "pages/about"
   
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
