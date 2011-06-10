5abc6293eb49ca8b6611ca50e5a37687a000e0a5
Gregoire Hibon <spermo9@gmail.com>
2011-06-10 19:48:55 +0800

    sessions terminé

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,24 @@ class UsersController < ApplicationController
   
   def new
     @title = "Registration"
+    @user  = User.new
   end
   
   def show
     @user = User.find(params[:id])
   end
 
+  def create
+      @user = User.new(params[:user])
+      if @user.save
+        sign_in @user
+        flash[:success] = "Welcome to Automoto!"
+        redirect_to @user
+      else
+        @title = "Sign up"
+        render 'new'
+      end
+   end
+   
+   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Automoto::Application.routes.draw do
   
   resources :users
-
+  resources :sessions,   :only  => [:new, :create, :destroy]
   root :to => 'pages#home'
   
   match '/contact',    :to => 'pages#contact'
   match '/about',      :to => 'pages#about'
   match '/register',   :to => 'users#new'
-
   
+  match '/signin',     :to => 'sessions#new'
+  match '/signout',    :to => 'sessions#destroy'
   
 end
